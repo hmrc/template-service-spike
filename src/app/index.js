9209c85dd596e7ad3a8adc84d299842702147cd2
Nat Carey <nat@dorightdigital.com>
2020-04-16 16:07:04 +0100

    Moved routing into app.  Previously the app contained one router which contained more routers.

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 
-const routes = require('./routes')
+const rootController = require('./renderers/root')
+const examplesController = require('./renderers/example')
+const govukDefaultTemplateController = require('./renderers/govukDefaultTemplate')
+const componentController = require('./renderers/component')
 
 const app = express()
 
-app.use(routes)
+app.use('/', rootController)
+app.use('/examples-output', examplesController)
+app.use('/govuk', govukDefaultTemplateController)
+app.use('/govuk', componentController('govuk'))
+app.use('/hmrc', componentController('hmrc'))
 
 module.exports = app
